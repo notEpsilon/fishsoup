6c7b70ab8dd4cea6da1790188b4c8142760e601f
notEpsilon <ibrahimnasr393@gmail.com>
2022-05-13 09:10:44 +0200

    implemented authentication using sanctum

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Validation\ValidationException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+
+        if (Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(Auth::user(), 200);
+        }
+
+        throw ValidationException::withMessages([
+            'email' => 'Invalid Email or Password'
+        ]);
+    }
+
+    public function logout() {
+        Auth::logout();
+    }
+}
---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => ['required'],
+            'email' => ['required', 'email', 'unique:users'],
+            'password' => ['required', 'min:8', 'confirmed']
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'type' => 0
+        ]);
+    }
+}
