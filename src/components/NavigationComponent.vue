e0b1290da75da92a9622567a101d3b27d4fb0fc3
notEpsilon <ibrahimnasr393@gmail.com>
2022-05-16 09:54:03 +0200

    minor edits

--- src/components/NavigationComponent.vue
+++ src/components/NavigationComponent.vue
@@ -1,10 +1,8 @@
-<!-- This example requires Tailwind CSS v2.0+ -->
 <template>
   <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
     <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
       <div class="relative flex items-center justify-between h-16">
         <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-          <!-- Mobile menu button-->
           <disclosure-button class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
             <span class="sr-only">Open main menu</span>
             <menu-icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
