42adc5b30ff89035315098c517e5dc6a7b92460e
notEpsilon <ibrahimnasr393@gmail.com>
2022-05-17 10:22:25 +0200

    heroku 1

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: 'http://localhost:8000/api'
+    baseURL: 'https://fishsoupapp.herokuapp.com/api/' // http://localhost:8000/api , https://fishsoupapp.herokuapp.com/api/
 });
 
 API.defaults.withCredentials = true;
