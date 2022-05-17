b2109010193f592ed1c2ef4431881cf3694868ba
notEpsilon <ibrahimnasr393@gmail.com>
2022-05-17 10:54:42 +0200

    heroku 6

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import { join } from 'path';
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
@@ -7,6 +7,6 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 8080;
 
-app.use('/', express.static(join(__dirname, '/dist')));
+app.use('/', express.static(path.join(__dirname, '/dist')));
 
 app.listen(PORT, () => console.log(`app up & running on port: ${PORT}`));
