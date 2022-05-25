6722c50236ec08d8e1e8be6e6bc2e402a6bf23b7
notEpsilon <ibrahimnasr393@gmail.com>
2022-05-25 21:17:35 +0200

    upd 3

--- src/components/RenderOrder.vue
+++ src/components/RenderOrder.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="max-w-sm w-full lg:max-w-full lg:flex shadow">
         <div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" :style="{ backgroundImage: `url(${product?.image_link})` }"></div>
-        <div class="w-full border-r border-b border-l lg:border-l-0 lg:border-t bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal" :class="ord.status === 0 ? 'border-teal-400 lg:border-teal-400' : 'border-purple-400 lg:border-purple-400'">
+        <div class="w-full border-r border-b border-l lg:border-l-0 lg:border-t bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal" :class="ord.status === false ? 'border-teal-400 lg:border-teal-400' : 'border-purple-400 lg:border-purple-400'">
             <div class="mb-8 flex justify-between">
                 <div>
                     <p class="text-sm text-gray-600 flex items-center">
@@ -13,13 +13,13 @@
                     <div class="text-gray-900 font-bold text-xl mb-2">{{ product?.name }}</div>
                     <p :class="`text-gray-700 text-base ${!ord.street_address && 'text-red-400'}`">{{ ord.street_address ? ord.street_address : "No Address Provided" }}</p>
                 </div>
-                <button v-if="ord.status === 1" @click="removeItem" class="rounded-md py-2 w-32 lg:w-36 bg--500 h-[2.32rem] bg-rose-600 hover:bg-rose-500 text-white align-middle text-center text-sm" :class="worker ? 'bg-rose-400' : ''" :disabled="worker">
+                <button v-if="ord.status === true" @click="removeItem" class="rounded-md py-2 w-32 lg:w-36 bg--500 h-[2.32rem] bg-rose-600 hover:bg-rose-500 text-white align-middle text-center text-sm" :class="worker ? 'bg-rose-400' : ''" :disabled="worker">
                     <span class="flex items-center justify-center">
                         <x-icon class="h-5" />
                         <span class="ml-[0.2rem] mr-1">Remove</span>
                     </span>
                 </button>
-                <button v-if="worker && ord.status !== 1" @click="markAsDone" class="rounded-md py-2 w-32 lg:w-36 bg--500 h-[2.32rem] bg-rose-600 hover:bg-rose-500 text-white align-middle text-center text-sm">
+                <button v-if="worker && ord.status !== true" @click="markAsDone" class="rounded-md py-2 w-32 lg:w-36 bg--500 h-[2.32rem] bg-rose-600 hover:bg-rose-500 text-white align-middle text-center text-sm">
                     Mark as Done
                 </button>
             </div>
@@ -28,7 +28,7 @@
                     <p class="text-gray-900 leading-none">Ordered by: {{ userOrdered?.name }}</p>
                     <p class="text-gray-600">Ordered on: {{  new Date(ord.created_at).toDateString() }}</p>
                 </div>
-                <badge-component :text="ord.status === 0 ? 'In Progress' : 'Done'" :done="ord.status === 0 ? false : true" />
+                <badge-component :text="ord.status === false ? 'In Progress' : 'Done'" :done="ord.status === false ? false : true" />
             </div>
         </div>
     </div>
